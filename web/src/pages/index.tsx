74daa611240c6d48ef6ab282e65ec9f1e0ec4963
Al3Xoni <togoialexandru98@gmail.com>
2026-02-18 21:37:45 +0200

    update_14

---
+++ web/src/pages/index.tsx
@@ -0,0 +1,8 @@
+export default function Home() { 
+  return (
+    <div style={{background: "black", color: "white", padding: "100px"}}>
+      <h1>[PAGES ROUTER STATUS: OK]</h1>
+      <p>If you see this, the problem is in the App Router configuration.</p>
+    </div>
+  ); 
+}
